e8da9ade3ea60d28e53ae90484cad39c1e1555c7
Андрей <@Asus-551l>
2014-11-09 00:35:59 +0400

    update

--- Java-DeveloperStarted/src/ru/ignatevandrej/lvl_1/work_001/sours/Main.java
+++ Java-DeveloperStarted/src/ru/ignatevandrej/lvl_1/work_001/sours/Main.java
@@ -6,8 +6,6 @@
 package ru.ignatevandrej.lvl_1.work_001.sours;
 
 import java.io.*;
-
-
 /**
  * @author Игнатьев Андрей
  * 08.11.2014 23:00
@@ -20,6 +18,7 @@ public class Main {
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 			System.out.println("Hello World");
+			System.out.printf("Привет %s \n", "всем");
 			BufferedReader Reader = new BufferedReader
 					(new InputStreamReader(System.in));
 			System.out.println("Вы ввели: " + Reader.readLine());
