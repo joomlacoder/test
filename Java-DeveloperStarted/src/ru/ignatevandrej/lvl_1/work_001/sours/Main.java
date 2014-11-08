6382086f3e8ac09562cca57f83a1876cea747eb0
Андрей <@Asus-551l>
2014-11-09 00:01:16 +0400

    Start lvl 1 work 001

---
+++ Java-DeveloperStarted/src/ru/ignatevandrej/lvl_1/work_001/sours/Main.java
@@ -0,0 +1,19 @@
+/**
+ * YouTube / DeveloperStarted / Программирование на 
+ * Java #1 - введение  в java и java-техгологии
+ * Пример 001
+ */
+package ru.ignatevandrej.lvl_1.work_001.sours;
+/**
+ * @author Игнатьев Андрей
+ * 08.11.2014 23:00
+ */
+public class Main {
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			System.out.println("Hello World");
+	}
+}
